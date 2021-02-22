a031f79feddb1cb1a573d7bb69f2a67f7daba978
Thomas <thomas.carbillet@gmail.com>
2021-02-22 15:16:53 +0100

    feat: release version 1.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="django-gar",
-    version="1.3.0",
+    version="1.3.1",
     description="Handle login and ticket validation for french GAR",
     url="https://github.com/briefmnews/django-gar",
     author="Brief.me",
