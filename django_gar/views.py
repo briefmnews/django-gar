3a0d86408adc0640d1c06486bb10ddc71e7ad18e
Thomas <thomas.carbillet@gmail.com>
2022-05-23 13:31:54 +0200

    feat: log out user on /logout get request

--- django_gar/views.py
+++ django_gar/views.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 from django.conf import settings
 from django.contrib.sessions.backends.db import SessionStore
 from django.http import HttpResponse
+from django.template.response import TemplateResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
@@ -19,8 +20,15 @@ GAR_LOGOUT_URL = getattr(settings, "GAR_LOGOUT_URL", "/")
 
 @method_decorator(csrf_exempt, name="dispatch")
 class LogoutView(View):
+    def get(self, request, *args, **kwargs):
+        """logout the user is gar_logout is in session"""
+        if "gar_logout" in request.session:
+            self.request.session.delete()
+
+        return TemplateResponse(request, "django_gar/logout.html", {})
+
     def post(self, request, *args, **kwargs):
-        """x-www-form-urlencoded body to decode"""
+        """x-www-form-urlencoded body to decode from GARrequest logout"""
         body = request.body.decode("utf-8")
 
         logger.info(body)
@@ -34,12 +42,13 @@ class LogoutView(View):
             try:
                 gar_session = GARSession.objects.get(ticket=session_index.text)
                 store = SessionStore(session_key=gar_session.session_key)
-                store.clear()
-                logger.info(f"clearing GAR session {gar_session.session_key}")
+                logger.info(f"request GAR logout {gar_session.session_key}")
 
                 store["gar_logout"] = True
                 store.save()
             except GARSession.DoesNotExist:
-                logger.info("cannot delete GAR session as it does not exist anymore")
+                logger.info(
+                    "cannot log out from GAR session as it does not exist anymore"
+                )
 
         return HttpResponse(status=204)
