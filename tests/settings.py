ee787276f9cb3f87e70c30b7fa45e07ed40f5b4e
Thomas <tcarbillet@brief.me>
2024-08-30 17:36:15 +0200

    feat(settings): add USE_TZ = True

--- tests/settings.py
+++ tests/settings.py
@@ -20,3 +20,5 @@ GAR_BASE_URL = "https://idp-auth.partenaire.test-gar.education.fr/"
 GAR_QUERY_STRING_TRIGGER = "sso_id"
 GAR_ACTIVE_USER_REDIRECT = "/"
 GAR_INACTIVE_USER_REDIRECT = "/inactive/"
+
+USE_TZ = True
