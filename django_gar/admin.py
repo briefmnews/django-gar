f4d09cd23cb4cfd0ab9f20dff5a9c6c94bdacb15
Thomas <tcarbillet@brief.me>
2024-09-11 18:29:37 +0200

    refactor(admin): add gar_subscription_response in a readonly field instead of change_view

--- django_gar/admin.py
+++ django_gar/admin.py
@@ -1,33 +1,29 @@
 from django.contrib import admin
-from django.shortcuts import get_object_or_404
+from django.utils.safestring import mark_safe
 
 from .gar import get_gar_subscription
 from .forms import GARInstitutionForm
 from .models import GARInstitution
 
-
+@admin.register(GARInstitution)
 class GARInstitutionAdmin(admin.ModelAdmin):
     raw_id_fields = ("user",)
     list_display = ("institution_name", "user", "uai", "ends_at")
     list_select_related = ("user",)
-    readonly_fields = ("id_ent",)
+    readonly_fields = ("id_ent", "gar_subscription_response")
     ordering = ("institution_name",)
     search_fields = ("institution_name", "user__email", "uai", "project_code")
     list_filter = ["project_code"]
     form = GARInstitutionForm
 
-    def change_view(self, request, object_id, form_url="", extra_context=None):
-        obj = get_object_or_404(self.get_queryset(request), pk=object_id)
-        extra_context = extra_context or {}
+    @admin.display(description="Etat de l'abonnement dans le GAR")
+    def gar_subscription_response(self, obj):
         gar_subscription = get_gar_subscription(obj.uai, obj.subscription_id)
-        if gar_subscription:
-            extra_context["gar_subscription"] = gar_subscription
-        else:
-            extra_context["gar_subscription"] = (
+
+        if not gar_subscription:
+            return (
                 "L'abonnement n'existe pas dans le GAR. "
                 "Vous pouvez le supprimer et en créer un nouveau."
             )
-        return super().change_view(request, object_id, form_url, extra_context)
 
-
-admin.site.register(GARInstitution, GARInstitutionAdmin)
+        return mark_safe(f"<code>{gar_subscription[0]}</code>")
